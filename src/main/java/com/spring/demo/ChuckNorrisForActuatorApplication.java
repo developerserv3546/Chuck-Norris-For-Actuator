4409d3d423d77a64fa32031dafe1c895c5f7cde5
Serega <serv3546@gmail.com>
2018-10-24 16:00:51 -0700

    Initial commit

---
+++ src/main/java/com/spring/demo/ChuckNorrisForActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisForActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisForActuatorApplication.class, args);
+    }
+}
